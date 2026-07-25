d055f8f48eb7c651bac72d768801538c30091d05
E-Surat App <dev@univ.ac.id>
2026-07-25 20:39:18 +0700

    fix: update MahasiswaModel to use async db.get and db.query methods

--- src/models/MahasiswaModel.js
+++ src/models/MahasiswaModel.js
@@ -1,20 +1,18 @@
 const db = require('../../config/database');
 
 class MahasiswaModel {
-    static findByUserId(userId) {
-        const stmt = db.prepare('SELECT * FROM mahasiswa WHERE user_id = ?');
-        return stmt.get(userId);
+    static async findByUserId(userId) {
+        return await db.get('SELECT * FROM mahasiswa WHERE user_id = ?', [userId]);
     }
 
-    static findById(id) {
-        const stmt = db.prepare('SELECT * FROM mahasiswa WHERE id = ?');
-        return stmt.get(id);
+    static async findById(id) {
+        return await db.get('SELECT * FROM mahasiswa WHERE id = ?', [id]);
     }
 
-    static getAll() {
-        const stmt = db.prepare('SELECT * FROM mahasiswa ORDER BY nama_lengkap ASC');
-        return stmt.all();
+    static async getAll() {
+        return await db.query('SELECT * FROM mahasiswa ORDER BY nama_lengkap ASC');
     }
 }
 
 module.exports = MahasiswaModel;
+
